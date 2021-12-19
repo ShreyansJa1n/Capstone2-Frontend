30d1ee5a0f876bee8643fac20b568f2e2e931d9a
Shreyans Jain <shreyansjain2000@hotmail.com>
2021-12-19 15:51:56 +0530

    BugFixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
-import LoginLayout from "src/App.js";
+import LoginLayout from "App.js";
 
 ReactDOM.render(
   <BrowserRouter>
